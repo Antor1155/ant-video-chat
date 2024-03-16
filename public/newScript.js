30ad6559cd769c4c48aeffe2afa53737ba905ca0
Md Antor <md.antor1155@gmail.com>
2024-03-16 17:19:47 +0600

    making the call and answerign the call working perfectly

---
+++ public/newScript.js
@@ -0,0 +1,84 @@
+const socket = io("/")
+const peer = new Peer()
+const getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+const connectedPeers = {}
+
+// handeling and adding video to the browser 
+const videoGrid = document.getElementById("video-grid")
+const myVideo = document.createElement("video")
+myVideo.muted = true
+myVideo.autoplay = true
+const friendVideo = document.createElement("video")
+friendVideo.autoplay = true
+
+
+peer.on("open", id => {
+    console.log("my peer id: ", id)
+    socket.emit("join-room", roomId, id)
+})
+
+socket.on("user-connected", friendId => {
+    console.log("friend connected with id: ", friendId)
+
+    // making a call to friend 
+    makeStreamAndCall(friendId)
+})
+
+const makeStreamAndCall = friendId => {
+
+    getUserMedia({ video: true, audio: true }, stream => {
+
+        myVideo.srcObject = stream
+        videoGrid.append(myVideo)
+
+        console.log("making stream to call: ", stream)
+
+        // calling friend 
+        const call = peer.call(friendId, stream)
+
+        call.on("stream", remoteStream => {
+            friendVideo.srcObject = remoteStream
+            videoGrid.append(friendVideo)
+        })
+
+        call.on("close", ()=>{
+            friendVideo.remove()
+        })
+
+    },
+        err => {
+            console.log(err)
+        }
+
+    )
+}
+
+
+// answering a call 
+peer.on("call", call => {
+    console.log("answering a call")
+
+    getUserMedia({ video: true, audio: true }, stream => {
+        myVideo.srcObject = stream
+        videoGrid.append(myVideo)
+
+        call.answer(stream)
+
+        call.on("stream", remoteStream => {
+            friendVideo.srcObject = remoteStream
+            videoGrid.append(friendVideo)
+        }),
+
+            call.on("close", () => {
+                friendVideo.remove()
+            })
+
+        err => {
+            console.log(err)
+        }
+    })
+})
+
+
+// when user disconnects 
